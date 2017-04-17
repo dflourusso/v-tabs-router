83c42f4593280476122d26869a789fd6a47b4df4
Daniel Fernando Lourusso <dflourusso@gmail.com>
2017-04-17 09:46:57 -0300

    install test

--- src/index.js
+++ src/index.js
@@ -1,33 +1,29 @@
-import Tabs from './components/Tabs.vue'
-import Tab from './components/Tab.vue'
+import Tabs from 'Tabs.vue'
+import Tab from 'Tab.vue'
 
-export {
-  Tabs,
-  Tab
-}
+export { Tabs, Tab }
 
-
-const VTabsRouter = {
-  install (vue) {
-    vue.component('Tabs', Tabs)
-    vue.component('Tab', Tab)
-  }
-}
-if (typeof window !== 'undefined' && window.Vue) {
-    window.Vue.use(VTabsRouter)
-}
-export default VTabsRouter
-
-
-// function install(vue) {
-//   vue.component('Tabs', Tabs)
-//   vue.component('Tab', Tab)
+// const VTabsRouter = {
+//   install (vue) {
+//     vue.component('Tabs', Tabs)
+//     vue.component('Tab', Tab)
+//   }
 // }
-//
-// if (window.Vue) {
-//   Vue.use(install)
-// } else if (typeof exports === "object") {
-//   module.exports = install
-// } else if (typeof define === "function" && define.amd) {
-//   define([], function(){ return install })
+// if (typeof window !== 'undefined' && window.Vue) {
+//     window.Vue.use(VTabsRouter)
 // }
+// export default VTabsRouter
+
+
+function install(vue) {
+  vue.component('Tabs', Tabs)
+  vue.component('Tab', Tab)
+}
+
+if (window.Vue) {
+  Vue.use(install)
+} else if (typeof exports === "object") {
+  module.exports = install
+} else if (typeof define === "function" && define.amd) {
+  define([], function(){ return install })
+}
